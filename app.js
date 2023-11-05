c5fe6266049bed44951f8a38c3eab1086f2f189b
adeyombo <adeyomboadebola@gmail.com>
2023-11-05 10:23:31 +0100

    Core files added

---
+++ app.js
@@ -0,0 +1,23 @@
+const numbersInput = document.querySelector("#numbers");
+const getSumBtn = document.querySelector("#get-sum-btn");
+const total = document.querySelector("#sum");
+
+function isNumeric(value) {
+    return !isNaN(parseFloat(value)) && isFinite(value);
+}
+
+getSumBtn.onclick = (e) => {
+    e.preventDefault();
+
+    const numbers = numbersInput.value.split("\n").map(n => n.replace(/,/g, "")).filter(n => isNumeric(n)).map(n => Number(n));
+
+    console.log(numbers)
+
+    let sum = 0;
+
+    for (let i = 0; i < numbers.length; i++) {
+        sum += numbers[i];
+    }
+
+    total.value = sum;
+}
